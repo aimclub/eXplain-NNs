dffe80cb76445723b4b121ba9c0620a441cde03f
kseniia <ksynikulina@gmail.com>
2022-11-09 08:05:33 +0200

    uploaded bayesian network api

--- eXNN/NetBayesianization/api.py
+++ eXNN/NetBayesianization/api.py
@@ -1,31 +1,76 @@
 import torch
-from .wrap import create_bayesian_wrapper
+import torch.nn as nn
+import copy
+import statistics
+import torch.optim
+from torch.distributions import Beta
 
-def BasicBayesianPrediction(data: torch.Tensor, 
-                            model: torch.nn.Module,
-                            n_iter: int,
-                            mode: str,
-                            p: float,
-                            a: float,
-                            b: float):
-    return BasicBayesianWrapper(model, mode, p, a, b).predict(data, n_iter)
+#calculate mean and std after applying bayesian
+class NetworkBayesWrapper(nn.Module):
+    def __init__(self, 
+                 model: nn.Module, 
+                 dropout_p: float):
+        
+        super(NetworkBayesWrapper, self).__init__()
+        self.model = model
+        self.dropout_p = dropout_p
+         
+    def mean_forward(self, 
+                     data: torch.Tensor, 
+                     n_iter: int):
+        
+        results = []
+        out = {}
+        for i in range(n_iter):
+            model_copy = copy.deepcopy(self.model)
+            state_dict = model_copy.state_dict()
+            state_dict_v2 = copy.deepcopy(state_dict)
+            for key, value in state_dict_v2.items():
+                if 'weight' in key:
+                    output = nn.functional.dropout(value, self.dropout_p, training=True)
+                    state_dict_v2[key] = output
+            model_copy.load_state_dict(state_dict_v2, strict=True)
+            output = model_copy(data)
+            results.append(output)
+        result = [tensor.item() for tensor in results]
+        out['mean'] = round(statistics.mean(result), 3)
+        out['std'] = round(statistics.stdev(result), 3)
+        return out
+        
+    
 
-class BasicBayesianWrapper:
-    def __init__(self,
+#calculate mean and std after applying bayesian with beta distribution
+class NetworkBayesWrapperBeta(nn.Module):
+    def __init__(self, 
                  model: torch.nn.Module,
-                 mode: str,
-                 p: float,
-                 a: float,
-                 b: float):
-        self.model = create_bayesian_wrapper(model, mode, p, a, b)
-
-    def predict(self, data, n_iter):
-        all_probs = []
-        with torch.no_grad():
-            for i in range(n_iter):
-                prob = self.model(data).softmax(dim=1)
-                all_probs.append(prob)
-        all_probs = torch.stack(all_probs, dim=0)
-        all_preds = all_probs.argmax(dim=-1)
-        mean_preds = all_preds.mode(dim=0)[0]
-        return mean_preds
+                 alpha: float, 
+                 beta: float):
+        
+        super(NetworkBayesWrapperBeta, self).__init__()
+        self.model = model
+        self.alpha = alpha
+        self.beta = beta
+         
+    def mean_forward(self, 
+                     x: torch.Tensor, 
+                     n_iter: int):
+        
+        results = []
+        out = {}
+        m = Beta(torch.tensor(self.alpha), torch.tensor(self.beta))
+        p = m.sample()
+        for i in range(n_iter):
+            model_copy = copy.deepcopy(self.model)
+            state_dict = model_copy.state_dict()
+            state_dict_v2 = copy.deepcopy(state_dict)
+            for key, value in state_dict_v2.items():
+                if 'weight' in key:
+                    output = nn.functional.dropout(value, p, training=True)
+                    state_dict_v2[key] = output
+            model_copy.load_state_dict(state_dict_v2, strict=True)
+            output = model_copy(x)
+            results.append(output)
+        result = [tensor.item() for tensor in results]
+        out['mean'] = round(statistics.mean(result), 3)
+        out['std'] = round(statistics.stdev(result), 3)
+        return out  
