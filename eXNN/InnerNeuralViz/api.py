9f0b9e1ea8a306d9effce33cb14139d7ad22a8f6
tanyapole <tanusha2406@gmail.com>
2022-12-08 19:34:34 +0300

    improve plots

--- eXNN/InnerNeuralViz/api.py
+++ eXNN/InnerNeuralViz/api.py
@@ -5,8 +5,11 @@ import umap
 import plotly.express as px
 from eXNN.InnerNeuralViz.hook import get_hook
 
-def _plot(embedding):
-    return px.scatter(embedding[:,0], embedding[:,1])
+def _plot(embedding, labels):
+    if labels is not None:
+        return px.scatter(x=embedding[:,0], y=embedding[:,1], color=labels)
+    else:
+        return px.scatter(x=embedding[:,0], y=embedding[:,1])
 
 
 def ReduceDim(data: torch.Tensor,
@@ -22,15 +25,18 @@ def ReduceDim(data: torch.Tensor,
 def VisualizeNetSpace(model: torch.nn.Module,
                       mode: str,
                       data: torch.Tensor,
-                      layers: Optional[List[str]]=None):
+                      layers: Optional[List[str]]=None,
+                      labels: Optional[torch.Tensor]=None):
     if layers is None:
         layers = [_[0] for _ in model.named_children()]
+    if labels is not None:
+        labels = list(map(str, labels.detach().cpu().numpy().tolist()))
     hooks = {layer: get_hook(model, layer) for layer in layers}
     with torch.no_grad():
         out = model(data)
-    visualizations = {'input': _plot(ReduceDim(data, mode))}
+    visualizations = {'input': _plot(ReduceDim(data, mode), labels)}
     for layer in layers:
-        visualizations[layer] = _plot(ReduceDim(hooks[layer].fwd, mode))
+        visualizations[layer] = _plot(ReduceDim(hooks[layer].fwd, mode), labels)
     return visualizations
 
 def get_random_input(dims: List[int]):
