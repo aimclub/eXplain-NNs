10d59f4274e002d55530845d1a79b884c603e6b6
tanyapole <tanusha2406@gmail.com>
2022-12-13 22:55:12 +0300

    add docstrings

--- eXNN/InnerNeuralViz/api.py
+++ eXNN/InnerNeuralViz/api.py
@@ -1,6 +1,8 @@
 import math
+import numpy as np
 import torch
-from typing import List, Optional
+import plotly
+from typing import Dict, List, Optional
 from sklearn.decomposition import PCA
 import umap
 import plotly.express as px
@@ -14,7 +16,20 @@ def _plot(embedding, labels):
 
 
 def ReduceDim(data: torch.Tensor,
-              mode: str):
+              mode: str) -> np.ndarray:
+    """This function reduces data dimensionality to 2 dimensions.
+
+    Args:
+        data (torch.Tensor): input data of shape NxC1x...xCk, where N is the number of data points, C1,...,Ck are dimensions of each data point
+        mode (str): dimensionality reduction mode (`umap` or `pca`)
+
+    Raises:
+        ValueError: returned if unsupported mode is provided
+
+    Returns:
+        np.ndarray: data projected on a 2d space, of shape Nx2
+    """
+    
     data = data.detach().cpu().numpy().reshape((len(data), -1))
     if mode == 'pca':
         return PCA(n_components=2).fit_transform(data)
@@ -28,7 +43,21 @@ def VisualizeNetSpace(model: torch.nn.Module,
                       data: torch.Tensor,
                       layers: Optional[List[str]]=None,
                       labels: Optional[torch.Tensor]=None,
-                      chunk_size: Optional[int]=None):
+                      chunk_size: Optional[int]=None) -> Dict[str, plotly.graph_objs._figure.Figure]:
+    """This function visulizes data latent representations on neural network layers.
+
+    Args:
+        model (torch.nn.Module): neural network
+        mode (str): dimensionality reduction mode (`umap` or `pca`)
+        data (torch.Tensor): input data of shape NxC1x...xCk, where N is the number of data points, C1,...,Ck are dimensions of each data point
+        layers (Optional[List[str]], optional): list of layers for visualization. Defaults to None. If None, visualization for all layers is performed
+        labels (Optional[torch.Tensor], optional): data labels (colors). Defaults to None. If None, all points are visualized with the same color
+        chunk_size (Optional[int], optional): batch size for data processing. Defaults to None. If None, all data is processed in one batch
+
+    Returns:
+        Dict[str, plotly.graph_objs._figure.Figure]: dictionary with latent representations visualization for each layer
+    """
+    
     if layers is None:
         layers = [_[0] for _ in model.named_children()]
     if labels is not None:
@@ -56,6 +85,13 @@ def VisualizeNetSpace(model: torch.nn.Module,
             
 
 
-def get_random_input(dims: List[int]):
-    """Generates random data of dims=`dims` drawn from uniform distribution"""
+def get_random_input(dims: List[int]) -> torch.Tensor:
+    """This function generates uniformly distributed tensor of given shape.
+
+    Args:
+        dims (List[int]): required data shape
+
+    Returns:
+        torch.Tensor: uniformly distributed tensor of given shape
+    """
     return torch.rand(size=dims)
--- eXNN/NetBayesianization/api.py
+++ eXNN/NetBayesianization/api.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Dict, Optional
 import torch
 import torch.optim
 from eXNN.NetBayesianization.wrap import create_bayesian_wrapper
@@ -10,7 +10,21 @@ def BasicBayesianPrediction(data: torch.Tensor,
                             mode: str,
                             p: Optional[float]=None,
                             a: Optional[float]=None,
-                            b: Optional[float]=None):
+                            b: Optional[float]=None) -> Dict[str, torch.Tensor]:
+    """Function computes mean and standard deviation of bayesian equivalent of a neural network.
+
+    Args:
+        data (torch.Tensor): input data of shape NxC1x...xCk, where N is the number of data points, C1,...,Ck are dimensions of each data point
+        model (torch.nn.Module): neural network
+        n_iter (int): number of samplings form the bayesian equivalent of a neural network
+        mode (str): bayesianization method (`basic` or `beta`)
+        p (Optional[float], optional): parameter of dropout (for `basic` bayesianization). Defaults to None.
+        a (Optional[float], optional): parameter of beta distribution (for `beta` bayesianization). Defaults to None.
+        b (Optional[float], optional): parameter of beta distribution (for `beta` bayesianization). Defaults to None.
+
+    Returns:
+        Dict[str, torch.Tensor]: dictionary with `mean` and `std` of prediction
+    """
     return BasicBayesianWrapper(model, mode, p=p, a=a, b=b).predict(data, n_iter)
 
 class BasicBayesianWrapper:
@@ -20,10 +34,28 @@ class BasicBayesianWrapper:
                  p: Optional[float]=None,
                  a: Optional[float]=None,
                  b: Optional[float]=None):
+        """Class representing bayesian equivalent of a neural network.
+
+        Args:
+            model (torch.nn.Module): neural network
+            mode (str): bayesianization method (`basic` or `beta`)
+            p (Optional[float], optional): parameter of dropout (for `basic` bayesianization). Defaults to None.
+            a (Optional[float], optional): parameter of beta distribution (for `beta` bayesianization). Defaults to None.
+            b (Optional[float], optional): parameter of beta distribution (for `beta` bayesianization). Defaults to None.
+        """
         self.model = create_bayesian_wrapper(model, mode, p=p, a=a, b=b)
 
     
-    def predict(self, data, n_iter):
+    def predict(self, data, n_iter) -> Dict[str, torch.Tensor]:
+        """Function computes mean and standard deviation of bayesian equivalent of a neural network.
+
+        Args:
+            data (_type_): input data of shape NxC1x...xCk, where N is the number of data points, C1,...,Ck are dimensions of each data point
+            n_iter (_type_): number of samplings form the bayesian equivalent of a neural network
+
+        Returns:
+            Dict[str, torch.Tensor]: dictionary with `mean` and `std` of prediction
+        """
         res = self.model.mean_forward(data, n_iter)
         return {'mean': res[0], 'std': res[1]}
     
