d846bb61e25e164d6cc38c826a83ecd439c41295
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-07 18:15:18 +0400

    Update __init__.py

--- eXNN/visualization/__init__.py
+++ eXNN/visualization/__init__.py
@@ -1,2 +1,5 @@
-from .api import get_random_input, reduce_dim
-from .api import visualize_layer_manifolds, visualize_recurrent_layer_manifolds
+from .api import (
+  get_random_input, reduce_dim, 
+  visualize_layer_manifolds, 
+  visualize_recurrent_layer_manifolds
+)
