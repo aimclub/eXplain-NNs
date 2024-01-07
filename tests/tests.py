dacbc412bd0da4df65aa96d6e35c6c73a9ad2856
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-07 20:07:25 +0400

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -17,7 +17,7 @@ def test_check_random_input():
 
 def _check_reduce_dim(mode):
     N, dim, data = utils.create_testing_data()
-    reduced_data = viz_api.reduce_dim(data, mode)
+    reduced_data = viz_api.reduce_dim(data, mode, out_dim=2)
     utils.compare_values(np.ndarray, type(reduced_data), "Wrong result type")
     utils.compare_values((N, 2), reduced_data.shape, "Wrong result shape")
 
@@ -50,6 +50,26 @@ def test_visualization():
             f"Wrong value type for key {key}",
         )
 
+
+def test_embed_visualization():
+    N, dim, data = utils.create_testing_data()
+    model = utils.create_testing_model()
+    layers = ["second_layer", "third_layer"]
+    res = viz_api.visualize_recurrent_layer_manifolds(model, "umap", data, layers=layers)
+
+    utils.compare_values(dict, type(res), "Wrong result type")
+    utils.compare_values(3, len(res), "Wrong dictionary length")
+    utils.compare_values(
+        set(["input"] + layers),
+        set(res.keys()),
+        "Wrong dictionary keys",
+    )
+    for key, plot in res.items():
+        utils.compare_values(
+            matplotlib.figure.Figure,
+            type(plot),
+            f"Wrong value type for key {key}",
+        )
 
 def _test_bayes_prediction(mode: str):
     params = {"basic": dict(mode="basic", p=0.5), "beta": dict(mode="beta", a=0.9, b=0.2)}
