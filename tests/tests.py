2a46b710daf6ee777865a0617d44614828bdb5a1
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-07 21:45:40 +0400

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -55,7 +55,7 @@ def test_embed_visualization():
     N, dim, data, labels = utils.create_testing_data()
     model = utils.create_testing_model()
     layers = ["second_layer", "third_layer"]
-    res = viz_api.visualize_recurrent_layer_manifolds(model, "umap", 
+    res = viz_api.visualize_recurrent_layer_manifolds(model, "umap",
                                                       data, layers=layers, labels=labels)
     utils.compare_values(dict, type(res), "Wrong result type")
     utils.compare_values(3, len(res), "Wrong dictionary length")
