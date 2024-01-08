6b8c11619301eda1a6453868b676ceb5839da1a5
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-08 13:08:01 +0400

    Update api.py

--- eXNN/visualization/api.py
+++ eXNN/visualization/api.py
@@ -192,6 +192,8 @@ def visualize_recurrent_layer_manifolds(
         df = df.iloc[::arr_reducer, :]
         if heatmap is True:
             labels_noncat = labels
+            if labels_noncat[0].shape[0] > 1:
+                labels_noncat = np.argmax(labels_noncat, axis=1)
             center = np.zeros((len(np.unique(labels_noncat)), 3))
             med_dist = np.zeros((len(np.unique(labels_noncat)), len(np.unique(labels_noncat))))
             for i in range(len(np.unique(labels_noncat))):
