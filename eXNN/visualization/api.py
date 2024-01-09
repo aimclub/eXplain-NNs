5eb9a650a1b9632402403ae566d0290a2a865c97
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-09 22:17:06 +0400

    Update api.py

--- eXNN/visualization/api.py
+++ eXNN/visualization/api.py
@@ -208,7 +208,7 @@ def visualize_recurrent_layer_manifolds(
             autosize=False,
             width=1000,
             height=1000)
-        emb_out.close()
+        plt.close(emb_out)
         emb_viz[layer] = emb_out
     return emb_viz
 
