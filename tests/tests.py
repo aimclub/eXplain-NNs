3c5e69ae6005c7607de8f87199dcce7b5babd349
Pavel Sukachev <92699977+TheRealGremlin@users.noreply.github.com>
2024-01-09 20:42:46 +0400

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -32,7 +32,7 @@ def test_reduce_dim_pca():
 
 def test_visualization():
     N, dim, data = utils.create_testing_data()
-    model = utils.create_testing_model()
+    model = utils.create_testing_model_LSTM()
     layers = ["second_layer", "third_layer"]
     res = viz_api.visualize_layer_manifolds(model, "umap", data, layers=layers)
 
